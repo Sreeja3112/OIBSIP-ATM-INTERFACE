1a2b521ab2dc36512b5bc38500a95559e970cd5c
Sreeja3112 <122040134+Sreeja3112@users.noreply.github.com>
2023-04-17 22:33:07 +0530

    Update Atm.java

--- Atm.java
+++ Atm.java
@@ -1,74 +1,194 @@
-import java.util.Scanner;  
-  
+import java.util.Scanner;
 
-public class ATMInterface
-{  
-    
-    public static void main(String args[] )  
-    {  
-         
-        int balance = 100000, withdraw, deposit;  
-          
-        
-        Scanner sc = new Scanner(System.in);  
-          
-        while(true)  
-        {  
-            System.out.println("Automated Teller Machine");  
-            System.out.println("Choose 1 for Withdraw");  
-            System.out.println("Choose 2 for Deposit");  
-            System.out.println("Choose 3 for Check Balance");  
-            System.out.println("Choose 4 for EXIT");  
-            System.out.print("Choose the operation you want to perform:");  
-              
-            
-            int choice = sc.nextInt();  
-            switch(choice)  
-            {  
-                case 1:  
-        System.out.print("Enter money to be withdrawn:");  
-                      
-        
-        withdraw = sc.nextInt();  
-                      
-       
-        if(balance >= withdraw)  
-        {  
-            
-            balance = balance - withdraw;  
-            System.out.println("Please collect your money");  
-        }  
-        else  
-        {  
-              
-            System.out.println("Insufficient Balance");  
-        }  
-        System.out.println("");  
-        break;  
-   
-                case 2:  
-                      
-        System.out.print("Enter money to be deposited:");  
-                      
-        
-        deposit = sc.nextInt();  
-                      
-        
-        balance = balance + deposit;  
-        System.out.println("Your Money has been successfully depsited");  
-        System.out.println("");  
-        break;  
-   
-                case 3:  
-        
-        System.out.println("Balance : "+balance);  
-        System.out.println("");  
-        break;  
-   
-                case 4:  
-        
-        System.exit(0);  
-            }  
-        }  
-    }  
+class BankAccount {
+    int balance;
+    int transactionHistory;
+    String customerName;
+    String customerId;
+    int flag = 0;
+
+    BankAccount(String cName, String cId) {
+        customerName = cName;
+        customerId = cId;
+    }
+
+    public final void clrscr() {
+        try {
+            try {
+            final String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")) {
+                Runtime.getRuntime().exec("cls");
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (final Exception e) {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } 
+    } catch (final Exception es) {
+        // System.out.println("nothing here!");
+    }
+
+    }
+
+    void checkId() {
+        clrscr();
+        System.out.println("Welcome " + customerName);
+        System.out.println();
+        System.out.print("Please enter the Customer ID or PIN: ");
+        Scanner id = new Scanner(System.in);
+        String chk = id.nextLine();
+        if (chk.equals(customerId)) {
+            clrscr();
+            showMenu();
+        } else {
+            System.out.println("*****");
+            System.out.println("Wrong Login!!");
+            System.out.println("*****");
+
+            if (flag < 3) {
+                flag++;
+                checkId();
+            }
+        }
+    }
+
+    void deposit(int amount) {
+        if (amount != 0) {
+            balance = balance + amount;
+            prevTransaction = amount;
+        }
+    }
+
+    void withdraw(int amount) {
+        if (this.balance > amount) {
+            balance = balance - amount;
+            prevTransaction = -amount;
+        } else {
+            clrscr();
+            System.out.println("*****");
+            System.out.println("Sufficient Balance not available for the withdrawl!");
+            System.out.println("*****");
+        }
+    }
+
+    void gettransactionHistory() {
+        if (transactionHistory> 0) {
+            System.out.println("Deposited: " + transactionHistory);
+        } else if (transactionHistory < 0) {
+            System.out.println("Withdraw: " + Math.abs(transactionHistory));
+        } else {
+            System.out.println("No Transaction Occured ");
+        }
+    }
+
+    public void transfer(double amount, BankAccount acc) {
+        if (this.balance < amount) {
+            clrscr();
+            System.out.println("*****");
+            System.out.println("Transfer Fails due to insufficient balance!");
+            System.out.println("*****");
+        } else {
+            this.balance -= amount;
+            acc.balance += amount;
+            System.out.println("Account of " + this.customerName + " becomes $" + this.balance);
+            System.out.println("Account of " + acc.customerName + " becomes $" + acc.balance);
+            System.out.println("\n");
+        }
+    }
+
+    private void showMenu() {
+        char option;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome " + customerName);
+        System.out.println("Your ID is " + customerId);
+        do {
+            System.out.println("\n");
+            System.out.println("\n");
+            System.out.println("A. Check Balance");
+            System.out.println("B. Deposit");
+            System.out.println("C. Withdraw");
+            System.out.println("D. Transaction History");
+            System.out.println("E. Transfer");
+            System.out.println("F. Quit");
+
+            System.out.println("*****");
+            System.out.println("Enter the option");
+            System.out.println("*****");
+            option = sc.next().charAt(0);
+            option = Character.toUpperCase(option);
+            System.out.println("\n");
+
+            switch (option) {
+                case 'A':
+                    clrscr();
+                    System.out.println("*****");
+                    System.out.println("Balance " + balance);
+                    System.out.println("*****");
+                    System.out.println("\n");
+                    break;
+
+                case 'B':
+                    clrscr();
+                    System.out.println("*****");
+                    System.out.println("Enter the amount to deposit");
+                    System.out.println("*****");
+                    int amount = sc.nextInt();
+                    deposit(amount);
+                    System.out.println("\n");
+                    break;
+                
+                case 'C':
+                    clrscr();
+                    System.out.println("*****");
+                    System.out.println("Enter the amount to withdraw");
+                    System.out.println("*****");
+                    int amount2 = sc.nextInt();
+                    withdraw(amount2);
+                    System.out.println("\n");
+                    break;
+
+                case 'D':
+                    clrscr();
+                    System.out.println("*****");
+                    getPrevTransaction();
+                    System.out.println("*****");
+                    System.out.println("\n");
+                    break;
+
+                case 'E':
+                    clrscr();
+                    System.out.println("******");
+                    System.out.println("To whom");
+                    BankAccount bb = new BankAccount("Raj", "1002");
+                    System.out.println(bb.customerName);
+                    
+                    System.out.println("Amount to Transfer");
+                    double am = sc.nextDouble();
+                    
+                    transfer(am, bb);
+                    break;
+
+                case 'F':
+                    clrscr();
+                    System.out.println("***************");
+                    break;
+                
+                default:
+                    clrscr();
+                    System.out.println("Invalid Option!!! Please Enter Again");
+            }
+
+        } while (option != 'F');
+        System.out.println("ThankYou For using our services");
+
+    }
+
+
+
+    public static void main(String[] args) {
+        BankAccount ba = new BankAccount("Huzefa", "1001");
+        ba.checkId();
+    }
+
 }
